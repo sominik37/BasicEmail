2003ce496cbe4338a41b05e9cd73e8fd9f3ad799
sominik37 <somi_nik@yahoo.com>
2022-08-30 18:40:18 +0500

    Initial Commit

---
+++ Email.py
@@ -0,0 +1,36 @@
+import smtplib
+
+#Senders Info!
+Email = input("Enter Your Email: ")
+Pass = input("Enter Your Password: ")
+Name = input("Enter Your Name: ")
+
+sent_from = Email
+
+RecEmail = input("Send Email to: ")
+Subject = input("Subject: ")
+Message = input("Message: ")
+sent_to = RecEmail
+
+email_text = """\
+
+From: %s
+
+To: %s
+
+Subject: %s
+
+
+%s
+
+""" % (sent_from, ", ".join(sent_to), Subject, Message)
+
+try:
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.ehlo()
+    smtp_server.login(Email, Pass)
+    smtp_server.sendmail(Email, RecEmail, Message)
+    smtp_server.close()
+    print("Email sent Successfully!")
+except Exception as ex :
+    print("Failed!", ex)
